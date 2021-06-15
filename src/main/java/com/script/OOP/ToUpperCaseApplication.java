f4ebf4294bb01010490b15f39bdce8fcde7b592f
Federico Duratorre <federico.999@live.it>
2021-06-15 22:37:13 +0200

    Aggiunta del codice
    dopo aver importato il progetto ho modificato il codice in "src\main\java\com\script\OOP\ToUpperCaseApplication.java"

---
+++ src/main/java/com/script/OOP/ToUpperCaseApplication.java
@@ -0,0 +1,23 @@
+package com.script.OOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class ToUpperCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToUpperCaseApplication.class, args);
+	}
+
+	@GetMapping("/toUpperCase")
+
+	public String upCase(@RequestParam(value = "string", defaultValue = "stringa di default") String given) {
+
+		return given.toUpperCase();
+
+	}
+
+}
---
+++ src/test/java/com/script/OOP/ToUpperCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.script.OOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToUpperCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
